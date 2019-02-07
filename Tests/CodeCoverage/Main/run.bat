0cd3991f73c05d26cb0d51bb47f0e715c05d44a7
John Kouraklis <john.kouraklis@nusenvision.com>
2019-02-07 01:40:36 +0000

    + CodeCoverage Reports for Tests.Casbin

---
+++ Tests/CodeCoverage/Main/run.bat
@@ -0,0 +1,3 @@
+REM Execute in /Tests/CodeCoverage/Main
+
+..\CodeCoverage.exe -m ..\..\Main\Win32\Debug\Tests.Casbin.map -e ..\..\Main\Win32\Debug\Tests.Casbin.exe -html -xml -dproj ..\..\..\SourceCode\Packages\Casbin4D103Rio.dproj -uf filelist.txt -od reports
